82e93cd4c9e0602f63b489ccebcb80900b0ce95c
BQ <qian.brian@gmail.com>
2019-02-23 22:19:25 -0800

    fixed changeweight bug

--- client/src/utils/helper.js
+++ client/src/utils/helper.js
@@ -16,12 +16,12 @@ export const trainMaxToRM = function(tm) {
 export const swapTmRm = function(name, weight) {
   weight = parseInt(weight);
   if (name.includes('TM')) {
-    const returnObj = { name, weight };
+    const returnObj = { [name]: weight };
     name = name.split('TM')[0] + 'RM';
     const rmWeight = trainMaxToRM(weight);
     return { ...returnObj, [name]: rmWeight };
   } else if (name.includes('RM')) {
-    const returnObj = { name, weight };
+    const returnObj = { [name]: weight };
     name = name.split('RM')[0] + 'TM';
     const tmWeight = repMaxToTM(weight);
     return { ...returnObj, [name]: tmWeight };
